1fa13900fe702c6ba670abc092fec8bbecb8c357
zhao <zhaohttzy@163.com>
2019-04-29 20:09:16 +0800

    创建了新的分支11123132casefd

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,9 +5,6 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
-        System.out.println("efasfaefa");
-        System.out.println("nkjaefnkjasngksjg");
-        System.out.println("snjkafnfkajngklskgmlw");
 
     }
 
